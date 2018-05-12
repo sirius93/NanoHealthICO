273a73d01d50f3e6a71c11ff049aa72e31008db8
Nandan Kumar 2 <nankumar3@publicisgroupe.net>
2018-05-12 16:48:47 +0530

    initial commit with a small component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import HeroImageText from './hero-image-text';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeroImageText/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/hero-image-text.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './hero-image-text.css';
+
+class HeroImageText extends Component {
+  render() {
+    return (
+      <div className="Hero-image-text">
+        <header className="Hero-image-text-header">
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default HeroImageText;
