60f9c832fcd89430a5f454dba21f0c2517e74d5c
Ramasiva Challa <ramasiva.challa@nanohealth.in>
2018-05-22 17:16:47 +0530

    added hcc to our partners

--- src/components/our-partners/our-partners.js
+++ src/components/our-partners/our-partners.js
@@ -6,6 +6,7 @@ import empowerLabsLogo2x from '../../assets/images/Secondary-Logo-vector@2x.png'
 import ideaLabs from '../../assets/images/idea-labs.png';
 import ideaLabsLogo from '../../assets/images/idea-clinics.png';
 import ideaLabsLogo2x from '../../assets/images/idea-clinics@2x.png';
+import hccLogo from '../../assets/images/Hcc_logo.png';
 
 class OurTeam extends Component {
   render() {
@@ -32,11 +33,11 @@ class OurTeam extends Component {
                 </div>
               </div>
             </div> */}
-            <div className="col-xs-12 col-sm-6 col-md-6">
+            <div className="col-xs-12 col-sm-4 col-md-4">
               <div class="our-partners-section-container">
-                <div class="our-partners-image-container" href="/content/lcbo/en.html" target="_self">
+                {/*<div class="our-partners-image-container" href="/content/lcbo/en.html" target="_self">
                     <img class="our-partners-selector-image" src={ideaLabs} alt="empowerlabs" />
-                </div> 
+                </div> */}
                 <div class="our-partners-logo-name-container">
                   <picture>
                         <source media="(max-width: 767px)" srcset={ ideaLabsLogo + ' 1x' + ',' + ideaLabsLogo2x + ' 2x' } />
@@ -48,11 +49,11 @@ class OurTeam extends Component {
                 </div>
               </div>
             </div>
-            <div className="col-xs-12 col-sm-6 col-md-6">
+            <div className="col-xs-12 col-sm-4 col-md-4">
               <div class="our-partners-section-container">
-                <div class="our-partners-image-container" href="/content/lcbo/en.html" target="_self">
+                {/*<div class="our-partners-image-container" href="/content/lcbo/en.html" target="_self">
                     <img class="our-partners-selector-image" src={empowerLabs} alt="empowerlabs" />
-                </div> 
+                </div> */}
                 <div class="our-partners-logo-name-container">
                   <picture>
                         <source media="(max-width: 767px)" srcset={ empowerLabsLogo + ' 1x' + ',' + empowerLabsLogo2x + ' 2x' } />
@@ -61,6 +62,22 @@ class OurTeam extends Component {
                         <img class="our-partners-logo" src={empowerLabsLogo} alt="empowerlabs" />
                   </picture>
                   <p class="our-partners-name">Empower Labs</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xs-12 col-sm-4 col-md-4">
+              <div class="our-partners-section-container">
+                {/*<div class="our-partners-image-container" href="/content/lcbo/en.html" target="_self">
+                    <img class="our-partners-selector-image" src={empowerLabs} alt="empowerlabs" />
+                </div> */}
+                <div class="our-partners-logo-name-container">
+                  <picture>
+                        <source media="(max-width: 767px)" srcset={ hccLogo + ' 1x' + ',' + hccLogo + ' 2x' } />
+                        <source media="(max-width: 1025px)" srcset={ hccLogo + ' 1x' + ',' + hccLogo + ' 2x' } />
+                        <source srcset={ hccLogo + ' 1x' + ',' + hccLogo + ' 2x' }  />
+                        <img class="our-partners-logo" src={hccLogo} alt="hcc" />
+                  </picture>
+                  <p class="our-partners-name">Hyderabad Cricket Club</p>
                 </div>
               </div>
             </div>
