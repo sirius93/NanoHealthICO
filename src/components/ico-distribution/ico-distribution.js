d872583bbd388bdac66def373c2995c4d218b8ea
Nandan Kumar <nkumar101@sapient.com>
2018-06-29 15:11:22 +0530

    mobile UI fixed

--- src/components/ico-distribution/ico-distribution.js
+++ src/components/ico-distribution/ico-distribution.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import './ico-distribution.css';
 import UseofProceeds from '../../assets/images/group-20.png';
 import UseofProceeds2x from '../../assets/images/group-20@2x.png';
+import UseofProceedsmobile from '../../assets/images/group-20.svg';
 import NHTTokendis from '../../assets/images/group_20.png';
 import NHTTokendis2x from '../../assets/images/group_20@2x.png';
+import NHTTokendisMobile from '../../assets/images/group-20-copy.svg';
 import Wawa from '../../assets/images/group-21.png';
 import Wawa2x from '../../assets/images/group-21@2x.png';
 import Warma from '../../assets/images/group-21-copy.png';
@@ -19,34 +21,34 @@ class IcoDistribution extends Component {
             <div className="col-xs-12">
               <div className="ico-distribution-left-image">
                 <div className="row">
-                  <div className="col-xs-4 ico-distribution-image">
+                  <div className="col-xs-6 col-sm-4 col-md-4 ico-distribution-image">
                   <picture>
-                      <source media="(max-width: 767px)" srcset={ UseofProceeds + ' 1x' + ',' + UseofProceeds2x + ' 2x' } />
+                      <source media="(max-width: 767px)" srcset={ UseofProceedsmobile + ' 1x' + ',' + UseofProceedsmobile + ' 2x'} />
                       <source media="(max-width: 1025px)" srcset={ UseofProceeds + ' 1x' + ',' + UseofProceeds2x + ' 2x' } />
                       <source srcset={ UseofProceeds + ' 1x' + ',' + UseofProceeds2x + ' 2x' }  />
                       <img  src={UseofProceeds} alt="Use of Proceeds" />
                   </picture>
                     {/* <img src={UseofProceeds} alt="Use of Proceeds"/> */}
                   </div>
-                  <div className="col-xs-8 ico-distribution-text">
+                  <div className="col-xs-6 col-sm-8 col-md-8 ico-distribution-text">
                     <div className="col-xs-12">
                       <h2 className="ico-distribution-text-heading">
                         Use of Proceeds
                       </h2>
                     </div>
-                    <div className="col-xs-6">
+                    <div className="col-xs-12 col-sm-6 col-md-6">
                       <p>Tech Dev and Deploy</p>
                       <h5 className="tech-per">55%</h5>
                     </div> 
-                    <div className="col-xs-6">
+                    <div className="col-xs-12 col-sm-6 col-md-6">
                       <p>Sales, Biz Dev and Mktg</p>
                       <h5 className="biz-per">20%</h5>
                     </div>
-                    <div className="col-xs-6">
+                    <div className="col-xs-12 col-sm-6 col-md-6">
                       <p>Market expansion</p>
                       <h5 className="market-per">15%</h5>
                     </div>
-                    <div className="col-xs-6">
+                    <div className="col-xs-12 col-sm-6 col-md-6">
                       <p>Legal and Consulting</p>
                       <h5 className="legal-per">10%</h5>
                     </div> 
@@ -55,36 +57,36 @@ class IcoDistribution extends Component {
               </div>
               <div className="ico-distribution-right-image">
                 <div className="row">
-                <div className="col-xs-8 ico-distribution-text">
+                <div className="col-xs-6 col-sm-8 col-md-8  ico-distribution-text">
                     <div className="col-xs-12">
                       <h2 className="ico-distribution-text-heading">
                         NHCT Token Distribution
                       </h2>
                     </div>
-                    <div className="col-xs-4">
+                    <div className="col-xs-12 col-sm-4 col-md-4">
                       <p>Crowd Sale</p>
                       <h5 className="tech-per">35%</h5>
                     </div>
-                      <div className="col-xs-4">
+                      <div className="col-xs-12 col-sm-4 col-md-4">
                       <p className="biz">User rewards</p>
                     <h5 className="biz-per">30%</h5>
                     </div>
-                    <div className="col-xs-4">
+                    <div className="col-xs-12 col-sm-4 col-md-4">
                       <p className="market">ICO core team</p>
                       <h5 className="market-per">15%</h5>
                     </div>
-                    <div className="col-xs-4">
+                    <div className="col-xs-12 col-sm-4 col-md-4">
                       <p className="legal">Ecosystem development</p>
                       <h5 className="legal-per">18%</h5>
                     </div>
-                    <div className="col-xs-4">
+                    <div className="col-xs-12 col-sm-4 col-md-4">
                       <p className="legal">BOUNTY TOKENS</p>
                       <h5 className="legal-per">2%</h5>
                     </div>
                   </div>
-                  <div className="col-xs-4 ico-distribution-image">
+                  <div className="col-xs-6 col-sm-4 col-md-4 ico-distribution-image">
                   <picture>
-                      <source media="(max-width: 767px)" srcset={ NHTTokendis + ' 1x' + ',' + NHTTokendis2x + ' 2x' } />
+                      <source media="(max-width: 767px)" srcset={ NHTTokendisMobile + ' 1x' + ',' + NHTTokendisMobile + ' 2x' } />
                       <source media="(max-width: 1025px)" srcset={ NHTTokendis + ' 1x' + ',' + NHTTokendis2x + ' 2x' } />
                       <source srcset={ NHTTokendis + ' 1x' + ',' + NHTTokendis2x + ' 2x' }  />
                       <img  src={NHTTokendis} alt="NHT token distribution" />
